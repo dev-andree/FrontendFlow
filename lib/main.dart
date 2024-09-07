8eff0f0b4cfc785c0eb01924aa0bc48f058d6f17
dev-andree <164929880+dev-andree@users.noreply.github.com>
2024-09-06 22:33:19 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:app_mobile/src/autenticacao/login_screen.dart';
+import 'package:app_mobile/src/configuracao/cores_customizadas.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,8 +14,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Login',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 225, 255)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: CoresCustomizadas.corAppCustomizada,
+        ),
         useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white.withOpacity(0.75),
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: CoresCustomizadas.corAppCustomizada,
+        ),
       ),
       home: const LoginScreen(),
     );
